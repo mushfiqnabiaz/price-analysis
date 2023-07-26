0fb393ac64fd35497f365a6811b6eab29a4e55f8
mushfiqnabiaz <mushfiq.nabiaz@gmail.com>
2023-07-26 17:50:41 +0600

    deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,34 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 st.title("Bangladesh Product Price Analysis")
 
+
+df = pd.read_csv('data/price-list-2015-2023.csv')
+    
+# Function to create the countplot 
+def create_countplot(data, x):
+    plt.figure(figsize=(12, 6))
+    sns.countplot(data=data, x=x)
+    plt.xticks(rotation=90)
+    plt.xlabel('Commodity')
+    plt.ylabel('Count')
+    plt.title('Count of Each Commodity')
+    st.pyplot()
+    
+# Streamlit app
 def main():
-    st.title("Read CSV File in Streamlit")
-
-    # Load the CSV file from the "data" directory
-    df = pd.read_csv('data/price-list-2015-2023.csv')
-
-    # Display the DataFrame
-    st.dataframe(df)
+    st.title('Commodity Count Analysis')
+    
+    # Display a sample of the DataFrame (optional)
+    st.subheader('Sample Data')
+    st.dataframe(df.head())
+    
+    # Display the countplot
+    st.subheader('Count of Each Commodity')
+    create_countplot(df, 'Commodity')    
 
 if __name__ == "__main__":
     main()
