a0a56af2ec1212858835c7851a43e7d31bc6044b
mushfiqnabiaz <mushfiq.nabiaz@gmail.com>
2023-07-26 17:39:46 +0600

    test push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("Bangladesh Product Price Analysis")
+
+#datafreame
+st.write("This is Dataframe Demo")
+df = pd.DataFrame(
+    np.random.randn(50,20),
+    columns=('col %d' % i for i in range (20))
+)
+
+st.dataframe(df)
