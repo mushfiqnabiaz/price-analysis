d4b48555acfed4443ec25ad26ab49cf35fb6c048
mushfiqnabiaz <mushfiq.nabiaz@gmail.com>
2023-07-26 17:48:04 +0600

    deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ def main():
     st.title("Read CSV File in Streamlit")
 
     # Load the CSV file from the "data" directory
-    file_path = "data/price-list-2015-2023.csv"
-    df = pd.read_csv(file_path)
+    df = pd.read_csv('data/price-list-2015-2023.csv')
 
     # Display the DataFrame
     st.dataframe(df)
